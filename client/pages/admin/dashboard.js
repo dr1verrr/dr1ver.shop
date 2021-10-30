33d29ed95681661e7dbd115d60b8000ecc8ba5a3
dr1verrr <darfor2107@gmail.com>
2021-10-31 01:44:17 +0300

    Commit

--- client/pages/admin/dashboard.js
+++ client/pages/admin/dashboard.js
@@ -38,11 +38,11 @@ export default function AdminDashboard(req, res) {
   return mounted ? (
     <div className='container'>
       <form onSubmit={submitHandler}>
-        <table>
+        <table className='table'>
           <thead>
             <tr>
               {products[0] &&
-                Object.keys(products[0]).map(item => {
+                productKeys.map(item => {
                   return <td key={item}>{item}</td>
                 })}
             </tr>
@@ -69,6 +69,35 @@ export default function AdminDashboard(req, res) {
               })}
           </tbody>
         </table>
+        <style jsx>
+          {`
+            .table {
+              width: 100%;
+
+              margin-bottom: 20px;
+
+              border: 1px solid #dddddd;
+
+              border-collapse: collapse;
+            }
+
+            .table th {
+              font-weight: bold;
+
+              padding: 5px;
+
+              background: #efefef;
+
+              border: 1px solid #dddddd;
+            }
+
+            .table td {
+              border: 1px solid #dddddd;
+
+              padding: 5px;
+            }
+          `}
+        </style>
       </form>
     </div>
   ) : null
