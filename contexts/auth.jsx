17ead429bd603d6e6903ec810779f86f24023f9c
dr1verrr <darfor2107@gmail.com>
2022-02-03 00:04:03 +0300

    add global states

--- contexts/auth.jsx
+++ contexts/auth.jsx
@@ -14,31 +14,35 @@ export const AuthProvider = ({ children }) => {
   const privateCondition = privateRoutes.includes(router.pathname)
   const [cartData, setCartData] = useLocalStorage('cart-data', [])
   const [popup, setPopup] = useState({ login: false, register: false })
+  const [cartVisible, setCartVisible] = useState(false)
+  const [showModal, setShowModal] = useState({ visible: false, title: 'title', message: 'message' })
+  const [isDuplicate, setDuplicate] = useState(false)
 
   function checkRoute() {
-    if (privateCondition && !user) {
+    if (privateCondition && !loading && !user) {
       router.replace('/')
     }
   }
 
-  useEffect(checkRoute, [router.pathname, user])
+  useEffect(checkRoute, [router.pathname, user, loading])
+
+  async function loadUserFromCookies() {
+    try {
+      const token = await axios.get('/api/token').then(res => res.data)
+
+      if (token) {
+        //console.log("Got a token in the cookies, let's see if it is valid")
+        api.defaults.headers.Authorization = `Bearer ${token}`
+        const { data: user } = await api.get('/users/me')
+        if (user) setUser(user)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+    setLoading(false)
+  }
 
   useEffect(() => {
-    async function loadUserFromCookies() {
-      try {
-        const token = await axios.get('/api/token').then(res => res.data)
-
-        if (token) {
-          console.log("Got a token in the cookies, let's see if it is valid")
-          api.defaults.headers.Authorization = `Bearer ${token}`
-          const { data: user } = await api.get('/users/me')
-          if (user) setUser(user)
-        }
-      } catch (err) {
-        console.error(err)
-      }
-      setLoading(false)
-    }
     loadUserFromCookies()
   }, [])
 
@@ -47,11 +51,20 @@ export const AuthProvider = ({ children }) => {
       value={{
         isAuthenticated: !!user,
         user,
+        setUser,
         loading,
+        setLoading,
         cartData,
         setCartData,
         popup,
         setPopup,
+        loadUserFromCookies,
+        cartVisible,
+        setCartVisible,
+        showModal,
+        setShowModal,
+        isDuplicate,
+        setDuplicate,
       }}
     >
       {children}
