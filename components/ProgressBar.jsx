73feb3a8cbfb4e36c374386756a319d3d22fd1b7
dr1verrr <darfor2107@gmail.com>
2022-02-20 10:31:18 +0300

    progress bar when routing

---
+++ components/ProgressBar.jsx
@@ -0,0 +1,92 @@
+import { Router } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function ProgressBar() {
+  const [progress, setProgress] = useState('')
+
+  useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setProgress('start')
+    }
+    const end = () => {
+      console.log('finished')
+      setProgress('end')
+    }
+
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
+
+  return (
+    <div className='progress-bar'>
+      <div className='progress-bar-progress' active={`${progress}`}></div>
+      <style jsx>{`
+        .progress-bar {
+          position: relative;
+          z-index: 2000;
+          position: fixed;
+          top: 0;
+          left: 0;
+          opacity: 1;
+          height: 3px;
+          pointer-events: none;
+          width: 100%;
+        }
+
+        .progress-bar-progress {
+          transform-origin: bottom left;
+          transform: scaleX(0);
+          height: 100%;
+          background: #00ffff;
+          opacity: 1;
+        }
+
+        .progress-bar-progress[active='start'] {
+          animation: progress-start 0.5s ease;
+          transform: scaleX(0.5);
+        }
+
+        .progress-bar-progress[active='end'] {
+          animation: progress-end 1s ease;
+        }
+
+        @keyframes progress-start {
+          0% {
+            opacity: 0;
+          }
+
+          50% {
+            opacity: 1;
+          }
+
+          100% {
+            opacity: 1;
+          }
+        }
+
+        @keyframes progress-end {
+          0% {
+            transform: scaleX(0.5);
+          }
+
+          75% {
+            opacity: 1;
+            transform: scaleX(1);
+          }
+
+          100% {
+            transform: scaleX(1);
+            opacity: 0;
+          }
+        }
+      `}</style>
+    </div>
+  )
+}
