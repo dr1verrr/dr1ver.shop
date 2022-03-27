3b6b71b58579ff076ce9794558e18a83c2486012
dr1verrr <darfor2107@gmail.com>
2022-03-27 18:36:08 +0300

    Deploy

--- pages/api/category/[slug].js
+++ pages/api/category/[slug].js
@@ -5,10 +5,17 @@ export default async function handler(req, res) {
   const { slug } = req.query
 
   const getProducts = async () => {
-    const request = await api.get(slug === 'all' ? '/products' : `/categories/${slug}`)
-    const data = request.data
+    try {
+      const response = await api.get(slug === 'all' ? '/products' : `/categories/${slug}`)
+      const data = response.data
 
-    if (data) return res.status(200).json(data)
+      console.log(data)
+
+      return res.status(response.status).json(data)
+    } catch (err) {
+      console.log(err)
+      res.status(err.response.data.statusCode).json({ message: err.response.data.message })
+    }
   }
 
   return getProducts()
